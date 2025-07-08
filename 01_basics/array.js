df46bc1eed719abdb3881c7028bd6fa5a450c87f
Prince Rajput <princesingh7547@gmail.com>
2025-07-08 11:08:34 +0000

    array

---
+++ 01_basics/array.js
@@ -0,0 +1,9 @@
+/*In JavaScript, an array is an ordered list of values
+Each value is called an element, and each element has a numeric position in the array  known as its index ;
+Arrays in JavaScript are zero-indexed, meaning the first element is at index 0, the second at index 1, and so on.
+*/
+
+const arr=[1,2,3,4,5];
+const arr1=["Prince","Aman"]
+
+console.log(arr)
